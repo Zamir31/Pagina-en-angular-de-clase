b65d9aaacb5d60a57ed982c4ae70ffd2a18513a4
Zamir31 <noriegad258@gmail.com>
2023-02-21 21:40:09 -0600

    Segundo Commit

--- new-app/src/app/app.component.ts
+++ new-app/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new-app';
+  title = 'Creando una app';
+  subtitulo = 'Esta es la prueba de una app';
+  descripcion = 'Este es una tarjeta para Github';
+  enlace = 'https://github.com'; 
 }
