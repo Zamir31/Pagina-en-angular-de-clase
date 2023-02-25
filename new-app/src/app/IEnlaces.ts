c58c33baaba7e00ed3f75d1af0397e3d436d7257
Zamir31 <noriegad258@gmail.com>
2023-02-25 07:50:09 -0600

    Cuarto Commit

---
+++ new-app/src/app/IEnlaces.ts
@@ -0,0 +1,5 @@
+import { IEnlace } from "./IEnlace";
+
+export interface IEnlaces {
+    enlaces: IEnlace[];
+}
--- new-app/src/app/enlaces-paginas-web/enlaces-paginas-web.component.ts
+++ new-app/src/app/enlaces-paginas-web/enlaces-paginas-web.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { IEnlace } from '../IEnlace';
+import { IEnlaces } from '../IEnlaces';
 
 @Component({
   selector: 'app-enlaces-paginas-web',
   templateUrl: './enlaces-paginas-web.component.html',
   styleUrls: ['./enlaces-paginas-web.component.css']
 })
-export class EnlacesPaginasWebComponent implements IEnlace {
-  nombreLink: string = 'Google';
-  url: string = 'http://google.com/';
+export class EnlacesPaginasWebComponent implements IEnlace, IEnlaces {
+  nombreLink: string = '';
+  url: string = '';
+  enlaces: IEnlace[] = [
+    {
+      nombreLink: 'Google',
+      url: 'http://www.google.com',
+    },
+    {
+      nombreLink: 'Github',
+      url: 'http://www.github.com',
+    }
+  ];
+  
   
 }
